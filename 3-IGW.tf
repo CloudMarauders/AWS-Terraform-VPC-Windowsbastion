0fc5d2afbfcdb51bb23e032bef687c58fde325a1
CloudMarauders <cloudmarauders@gmail.com>
2024-10-29 18:08:03 -0500

    Update 3-IGW.tf

--- 3-IGW.tf
+++ 3-IGW.tf
@@ -1,9 +1,34 @@
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.CloudMaraduers-Prod.id
 
   tags = {
-    Name = "CloudMaraduers-Prod"
+    Name  = "CloudMaraduers-Prod"
     Owner = "CloudMaraduers"
-    ring = "prod"
+    ring  = "prod"
   }
+}
+
+
+resource "aws_route_table" "prod_public_route_table" {
+  vpc_id = aws_vpc.CloudMaraduers-Prod.id
+
+  tags = {
+    Name  = "CloudMaraduers-Prod-Public-Route-Table"
+    Owner = "CloudMaraduers"
+    ring  = "prod"
+  }
+}
+
+
+resource "aws_route" "prod_public_route_internet" {
+  route_table_id         = aws_route_table.prod_public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"              
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+
+resource "aws_route_table_association" "prod_public_subnet_association" {
+  subnet_id      = aws_subnet.Production-A-Pub.id
+  route_table_id = aws_route_table.prod_public_route_table.id
 }
